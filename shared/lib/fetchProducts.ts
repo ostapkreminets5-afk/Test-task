76b50218fdd08c7c92beccbc94e15af169ed6377
ostapkreminets5-afk <ostapkreminets5@gmail.com>
2026-05-24 12:11:09 +0300

    feat:add try catch

--- shared/lib/fetchProducts.ts
+++ shared/lib/fetchProducts.ts
@@ -3,10 +3,17 @@ import type { ProductsApiResponse } from "../../shared/base/ProductsApiResponse"
 import { PRODUCTS_API_URL } from "../../shared/const/PRODUCTS_API_URL";
 
 export const fetchProducts = async (): Promise<Product[]> => {
-  const response = await fetch(PRODUCTS_API_URL, { next: { revalidate: 300 } });
-  if (!response.ok) {
-    throw new Error(`Failed to fetch products: ${response.status}`);
+  try {
+    const response = await fetch(PRODUCTS_API_URL, { next: { revalidate: 300 } });
+    if (!response.ok) {
+      throw new Error(`Failed to fetch products: ${response.status}`);
+    }
+
+    const data: ProductsApiResponse = await response.json();
+    console.log(data);
+    return data.products;
+  } catch (error) {
+    console.log(error);
+    return [];
   }
-  const data: ProductsApiResponse = await response.json();
-  return data.products;
 };
